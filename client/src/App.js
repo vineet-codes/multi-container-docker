849c8b4e4b6ee0530ed3bdc95cda58c5abc9e238
vineet-codes <vineetwrites@gmail.com>
2021-05-26 20:02:06 +0100

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import OtherPage from './OtherPage';
+import Fib from './Fib';
+
+function App() {
+  return (
+    <Router>
+      <div className='App'>
+        <div>
+          <h1>Compute Fibbonaci value for a given index</h1>
+          <Route exact path='/' component={Fib} />
+          <Route path='/otherpage' component={OtherPage} />
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
